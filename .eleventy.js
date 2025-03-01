de7291ae5427d7c25f589d8f5c5eee96e2c0c3ad
Felix Dimmerling <ahoi@felixdim.com>
2025-03-01 21:47:59 +0100

    updated 11ty setup/config & github actions for page deploy

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,12 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.setServerOptions({
         port: 55660,
         showAllHosts: true, // Set this to true to access the server on your local network
+        watch: ["_site/**/*.css"], // Watch CSS files for changes
     });
 
     eleventyConfig.addPassthroughCopy("./assets/.");
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/fonts/bootstrap-icons" })
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/fonts/bootstrap-icons" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/masonry-layout/dist/masonry.pkgd.min.js": "assets/lib/masonry-layout/masonry.pkgd.min.js" });
 
     return {
         dir: {
